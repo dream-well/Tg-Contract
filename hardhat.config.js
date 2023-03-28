d6ce20ec0c05a4f98d618e5a2b3035478f5e4a92
dream-well <dreamwell506@outlook.com>
2023-03-28 18:15:42 +0900

    remove waffle

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 
 require("@nomiclabs/hardhat-etherscan");
-require("@nomiclabs/hardhat-waffle");
 require("hardhat-gas-reporter");
 require("solidity-coverage");
 require('hardhat-contract-sizer');
